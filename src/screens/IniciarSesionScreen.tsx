5fded92cdd8645ae81ecc25ca905e4440bb12561
kevincadena <kevgeek5@gmail.com>
2026-02-23 07:31:22 -0500

    Merge faltante

--- src/screens/IniciarSesionScreen.tsx
+++ src/screens/IniciarSesionScreen.tsx
@@ -1,56 +1,106 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Image, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { stylesGlobal } from '../theme/AppTheme'
 import { CommonActions, useNavigation } from '@react-navigation/native';
+import { ButtonMonstrar } from '../components/ButtonMonstrar';
+import { InputComponent } from '../components/InputComponent';
+import { ButtonComponent } from '../components/BottonComponent';
+import { ButtonRSocialComponent } from '../components/ButtonRSocialComponent';
+import { BodyComponent } from '../components/BodyComponent';
+
+interface FormRegistro {
+
+    correo: string;
+    contrasena: string;
+
+}
 
 
 export const IniciarSesionScreen = () => {
     const navigation = useNavigation();
 
+    const [formRegistro, setFormRegistro] = useState<FormRegistro>({
+
+        correo: '',
+        contrasena: '',
+
+    });
+
+    const handleChangeValue = (name: string, value: string) => {
+        setFormRegistro({
+            ...formRegistro,
+            [name]: value
+        });
+    }
+
+    const handleRegistro = () => {
+        console.log('Datos de registro:', formRegistro);
+    }
+
+    
+    const [showPassword, setShowPassword] = useState(true);
+
+
+    const ChangeShowText = () => {
+        return showPassword ? 'Mostrar Contraseña' : 'Ocultar Contraseña';
+        
+    }
+
+
     return (
-        <View style={stylesGlobal.containerBody}>
-            <Text style={[stylesGlobal.text, stylesGlobal.title]}>Inicio de Sesión</Text>
-            <Text style={[stylesGlobal.text, stylesGlobal.subtitulo]}>¡Bienvenido/a de nuevo!</Text>
+        <BodyComponent>
+            <View style={stylesGlobal.containerBody}>
+                <Text style={[stylesGlobal.text, stylesGlobal.title]}>Inicio de Sesión</Text>
+                <Text style={[stylesGlobal.text, stylesGlobal.subtitulo]}>¡Bienvenido/a de nuevo!</Text>
 
-            <View style={[stylesGlobal.inputs]}>
-                <TextInput style={stylesGlobal.buttonText} placeholder='Correo Electrónico' placeholderTextColor='#7c7c7c'></TextInput>
-            </View>
+                <View style={stylesGlobal.containerInput}>
 
-            <View style={[stylesGlobal.inputs]}>
-                <TextInput style={stylesGlobal.buttonText} placeholder='Contraseña' placeholderTextColor='#7c7c7c'></TextInput>
-            </View>
+                    <InputComponent
+                        placeholder='Correo Electrónico'
+                        placeholderTextColor='#7c7c7c'
+                        handleChangeValue={handleChangeValue}
+                        name='correo'
+                        keyboardType='email-address'
+                    />
+
+                    
+                    <InputComponent
+                        placeholder='Contraseña'
+                        placeholderTextColor='#7c7c7c'
+                        handleChangeValue={handleChangeValue}
+                        name='contrasena'
+                        keyboardType='default'
+                        secureTextEntry={showPassword}
+                    />
+                    <ButtonMonstrar
+                        buttonText={ChangeShowText()}
+                        onPress={() => setShowPassword(!showPassword)}/>
+                    
 
 
-            <TouchableOpacity style={[stylesGlobal.inputs]}>
-                <Text style={stylesGlobal.buttonText}>INICIAR SESIÓN</Text>
-            </TouchableOpacity>
+                </View>
 
-            <Text style={stylesGlobal.text}>O Inicia Sesión con:</Text>
+                <ButtonComponent
+                    buttonText='INICIAR SESIÓN'
+                    onPress={handleRegistro}
+                />
 
-            <View style={stylesGlobal.buttonOtrasRedes}>
-                <TouchableOpacity>
-                    <Image source={{ uri:'https://i.postimg.cc/gkb2XygJ/icono-google.png' }} style={{ width: 40, height: 40 }}/>
+                <Text style={stylesGlobal.text}> O Inicia Sesión con:</Text>
+
+                <View style={stylesGlobal.buttonOtrasRedes}>
+                    <ButtonRSocialComponent
+                        uri='https://i.postimg.cc/gkb2XygJ/icono-google.png' />
+                    <ButtonRSocialComponent
+                        uri='https://i.postimg.cc/y8v1bMBt/icono-facebook.png' />
+                    <ButtonRSocialComponent
+                        uri='https://i.postimg.cc/d0BbyyLY/discord-logo.png' />
+                </View>
+
+                <TouchableOpacity onPress={() => navigation.dispatch(CommonActions.navigate({ name: 'RegistroScreen' }))}>
+                    <Text style={[stylesGlobal.text, stylesGlobal.textRegistro]}>¿No tienes cuenta? Registrate aquí</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity>
-                    <Image source={{ uri:'https://i.postimg.cc/y8v1bMBt/icono-facebook.png' }} style={{ width: 40, height: 40 }}/>
-                </TouchableOpacity>
-
-                <TouchableOpacity>
-                    <Image source={{ uri:'https://i.postimg.cc/fbPwwYVm/icono-discord.png' }} style={{ width: 40, height: 30 }}/>
-                </TouchableOpacity>
             </View>
-            
-            <TouchableOpacity>
-                <Text style={[stylesGlobal.text, stylesGlobal.textRegistro]}>Ayudada, no puedo iniciar sesión</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity onPress={() => navigation.dispatch(CommonActions.navigate({ name: 'RegistroScreen' }))}>
-                <Text style={[stylesGlobal.text, stylesGlobal.textRegistro]}>¿No tienes cuenta? Registrate aquí</Text>
-            </TouchableOpacity>
-
-
-        </View>
+        </BodyComponent>
     )
 }
-
