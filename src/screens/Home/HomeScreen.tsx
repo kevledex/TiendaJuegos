b6fb042b05905c112eaff6c80065ae0133a7c985
kevincadena <kevgeek5@gmail.com>
2026-02-25 19:04:26 -0500

    estructura del HomeScreen y CardProduct

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,15 +1,46 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import { BodyComponent } from '../../components/BodyComponent'
+import React, { useState } from 'react'
+import { FlatList, Text, View } from 'react-native'
+import { BodyComponent } from '../../components/BodyComponent';
+import { CardProductComponent } from './components/CardProductComponent';
+import Icon from '@expo/vector-icons/MaterialIcons';
+
+
+export interface Product{
+    id: number,
+    name: string,
+    genre: string,
+    developer: string, 
+    price: number,
+    pathImage: string,
+}
 
 export const HomeScreen = () => {
-  return (
-    <BodyComponent>
-    
+
+    //datos de prueba
+    const products: Product[] = [
+        {id: 1, name: 'Grand Theft Auto V ', genre:'Acción-Aventura', developer:' Rockstar Games', price: 35, pathImage: 'https://static.wikia.nocookie.net/esgta/images/1/1b/Car%C3%A1tula_GTA_V.jpg/revision/latest?cb=20130402191528'},
+        {id: 2, name: 'Forza Horizon 4 ', genre:'Carreras', developer:'Playground Games', price: 30, pathImage: 'https://media.vandal.net/m/59799/forza-horizon-4-2018102103227_1.jpg'},
+        {id: 3, name: 'Halo: The Master Chief Collection', genre:'FPS', developer:'343 Industries', price: 40, pathImage: 'https://images.g2a.com/360x600/1x1x1/halo-the-master-chief-collection-pc-steam-clave-global-i10000008375007/5f1aad3146177c782a0f91b3'},
+        {id: 4, name: 'Detroit: Become Human', genre:'Aventura gráfica', developer:'Quantic Dream', price: 40, pathImage: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ee/Detroit_Become_Human.jpg/250px-Detroit_Become_Human.jpg'},
+        {id: 5, name: 'Resident Evil 4 (Remake)', genre:'Survival Horror', developer:'Capcom', price: 50, pathImage: 'https://image.api.playstation.com/vulcan/ap/rnd/202210/0706/EVWyZD63pahuh95eKloFaJuC.png'},
+        {id: 6, name: 'Minecraft', genre:'Sandbox', developer:'Mojang Studios', price: 20, pathImage: 'https://areajugones.sport.es/wp-content/uploads/2022/01/minecraft-464x588.jpg.webp'},
+        {id: 7, name: 'God of War (2018)', genre:'Acción-Aventura', developer:'Santa Monica Studio', price: 50, pathImage: 'https://static.wikia.nocookie.net/godofwar/images/a/a8/Portada_God_of_War_%282018%29.png/revision/latest?cb=20211008000250&path-prefix=es'},
+        {id: 8, name: 'Hollow Knight', genre:'Metroidvania', developer:'Team Cherry', price: 20, pathImage: 'https://i.3djuegos.com/juegos/11596/hollow_knight/fotos/ficha/hollow_knight-3915488.jpg'},
+      ];
+
+    return (
     <View>
-        <Text>HomeScreen</Text>
+        
+        <BodyComponent>
+            <FlatList 
+                data={products}
+                //renderItem={({ item }) => <Text>{item.name}</Text>}
+                renderItem={({ item }) => <CardProductComponent item={item} />}
+                keyExtractor = {item => item.id.toString()}
+                numColumns={2}
+                columnWrapperStyle={{justifyContent: 'space-between'}}
+            />
+        </BodyComponent>
     </View>
-    
-    </BodyComponent>
-  )
+    )
 }
--- src/screens/Home/components/CardProductComponent.tsx
+++ src/screens/Home/components/CardProductComponent.tsx
@@ -1,13 +1,44 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Image, Text, View } from 'react-native'
+import { Product } from '../HomeScreen';
+import Icon from '@expo/vector-icons/MaterialIcons';
+import { SECONDARY_COLOR } from '../../../common/const';
+import { stylesGlobal } from '../../../theme/AppTheme';
 
-export const CardProductComponent = () => {
+
+interface Props {
+    item: Product;
+
+}
+
+
+export const CardProductComponent = ({item}: Props) => {
+    //hook usestate: gestionar el estado del modal
+    const [showModal, setShowModal] = useState<boolean>(false);
+
+    //funcion para mostara o ocultar el modal
+    const hiddenModal = (): void => {
+        setShowModal(!showModal);
+    }
+
     return (
-        <View>
-
-            <Text>
-
-            </Text>
-        </View>
+        //framme
+        <> 
+        <View >
+            <Image source={{uri: item.pathImage}}/>
+            <View>
+                <Text>{item.name}</Text>
+                <Text >${item.price.toFixed(2)}</Text>
+            </View>
+            <View style={{margin: 15}}>
+                <Icon name='add-shopping-cart' 
+                size={33} 
+                color={SECONDARY_COLOR}
+                onPress={hiddenModal}
+                />  
+            </View>          
+            </View>
+        
+        </> 
     )
 }
